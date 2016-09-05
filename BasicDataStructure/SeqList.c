1137d545da07e5ad7ef693e3a7edbb424f39d9c9
teddyxiong53 <1073167306@qq.com>
2016-09-05 22:53:41 +0800

    first commit, add SeqList Code

---
+++ BasicDataStructure/SeqList.c
@@ -0,0 +1,104 @@
+
+
+void SeqListInit(SeqListType *SL)
+{
+    SL->ListLen = 0;
+}
+int SeqListLength(SeqListType *SL)
+{
+    return SL->ListLen;
+}
+int SeqListAdd(SeqListType *SL, DATA data)
+{
+    if(SL->ListLen >= MAXSIZE)
+    {
+        printf("The SeqList is full \n");
+        return 0;
+    }
+    SL->ListLen ++;
+    printf("\nxhl -- len:%d\n", SL->ListLen);
+    SL->ListData[SL->ListLen] = data;
+    return 1;
+}
+
+int SeqListInsert(SeqListType * SL, int n, DATA data)
+{
+    int i=0;
+    if(SL->ListLen >= MAXSIZE)
+    {
+        printf("The SeqList is full \n");
+        return 0;
+    }
+    if((n < 1) || (n > SL->ListLen-1))
+    {
+        printf("Wrong Location to insert\n");
+        return 0;
+    }
+    for(i=SL->ListLen; i>=n;i--)
+    {
+        SL->ListData[i+1] = SL->ListData[i];
+    }
+    SL->ListData[n] = data;
+    SL->ListLen ++;
+    return 1;
+}
+
+
+int SeqListDelete(SeqListType * SL, int n)
+{
+    int i = 0;
+    if((n < 1) || (n > SL->ListLen+1))
+    {
+        printf("Wrong Location to delete \n");
+        return 0;
+    }
+    for(i=n; i<SL->ListLen;i++)
+    {
+        SL->ListData[i] = SL->ListData[i+1];
+    }
+    SL->ListLen --;
+    return 1;
+}
+
+
+DATA * SeqListFindByNum(SeqListType *SL, int n)
+{
+    if((n < 1) || (n > SL->ListLen+1))
+    {
+        printf("Wrong Location to find \n");
+        return NULL;
+    }
+    
+    return &(SL->ListData[n]);
+}
+
+int SeqListFindByCont(SeqListType *SL, char *key)
+{
+    int i =0;
+    for(i=1; i<=SL->ListLen; i++)
+    {
+        printf("\nlist:%s--input:%s\n", SL->ListData[i].key, key);
+        if(strcmp(SL->ListData[i].key, key) == 0)
+        {
+            //at least 1
+            return i;
+        }
+    }
+    
+    //0 mean an invalid index
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BasicDataStructure/SeqListTest.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+typedef struct 
+{
+    char key[15];
+    char name[20];
+    int age;
+} DATA;
+
+#include "SeqList.h"
+#include "SeqList.c"
+
+int SeqListAll(SeqListType *SL)
+{
+    int i=0;
+    for(i=1;i<=SL->ListLen;i++)
+    {
+        printf("\n(%s,%s,%d) \n", SL->ListData[i].key,SL->ListData[i].name, SL->ListData[i].age);
+    }
+    return 0;
+}
+
+
+int main()
+{
+    int i;
+    SeqListType SL;
+    DATA data, *data1;
+    char key[15];
+    //init SeqList
+    SeqListInit(&SL);
+    do
+    {
+        printf("please input No Name Age :");
+        fflush(stdin);
+        scanf("%s%s%d", &data.key, &data.name, &data.age);
+        if(data.age != 0)
+        {
+            if(!SeqListAdd(&SL, data))
+            {
+                break;
+            }
+        }
+        else
+        {
+            
+            break;
+        }
+    }while(1);
+    printf("\n------Content In the SeqList---------\n");
+    SeqListAll(&SL);
+    fflush(stdin);
+
+    printf("\n-------Test get by num- ------\n");
+    printf("please input a num:\n");
+    scanf("%d",&i);
+    data1 = SeqListFindByNum(&SL, i);
+    if(data1)
+    {
+        printf("\n%d content is(%s %s %d)\n", i ,data1->key, data1->name, data1->age);
+    }
+    else
+    {
+        printf("\n%d not found\n");
+    }
+    fflush(stdin);
+
+    printf("\n-------Test get by content- ------\n");
+    scanf("%s",key);
+    i = SeqListFindByCont(&SL, key);
+    if(i)
+    {
+        printf("\n%s content is(%s %s %d)\n",key,  SL.ListData[i].key, SL.ListData[i].name, SL.ListData[i].age);
+    }
+    else
+    {
+        printf("\n%s not found\n", key);
+    }
+    fflush(stdin);
+    getchar();
+    return 0;
+}
